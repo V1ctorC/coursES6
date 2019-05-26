8a0c1dd6877182e79b74d549646ac4434bbe940c
Victor <victor.du.77@hotmail.fr>
2019-05-26 20:41:15 +0200

    Course 6, const VS let

--- play.js
+++ play.js
@@ -1,12 +1,16 @@
-console.log(aGreatNumber);
-var aGreatNumber = 10;
+const aGreatNumber = 10;
+const aGreatObject = {
+    withGreatKey: true
+};
 
 if (true){
-    aGreatNumber = 42;
+    //aGreatNumber = 42;
+    aGreatObject.withGreatKey = false;
 }
 
 setTimeout(() =>{
-    console.log(aGreatNumber)
+    console.log(aGreatNumber);
+    console.log(aGreatObject);
 }, 1000);
 
 console.log('Waiting ...');
